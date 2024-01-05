9fe0cb3139be4b56498aa0bec5b4ece4c2db5083
vaishnav-sic <vaishnavwirkar2011@gmail.com>
2024-01-05 08:44:26 +0530

    Minor Changes

--- files/SAComponents/SAFooter.js
+++ files/SAComponents/SAFooter.js
@@ -8,7 +8,7 @@ const SAFooter = () => {
       <div className="map">
         <iframe
           title="Location Map"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30444.355594855253!2d74.19480522778635!3d17.481504822928244!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc22b4b03f8155b%3A0x263c77e3d58e9bd3!2sGaikwadwadi%2C%20Maharashtra!5e0!3m2!1sen!2sin!4v1703315312103!5m2!1sen!2sin" allowfullscreen="" referrerpolicy="no-referrer-when-downgrade"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d30444.355594855253!2d74.19480522778635!3d17.481504822928244!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc22b4b03f8155b%3A0x263c77e3d58e9bd3!2sGaikwadwadi%2C%20Maharashtra!5e0!3m2!1sen!2sin!4v1703315312103!5m2!1sen!2sin" allowFullScreen="" referrerPolicy="no-referrer-when-downgrade"
           width="300"
           height="200"
           style={{ border: 0 }}
--- files/nimantran/Footer.js
+++ files/nimantran/Footer.js
@@ -8,7 +8,7 @@ const SAFooter = () => {
       <div className="map">
         <iframe
           title="Location Map"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d241662.16175808158!2d73.80454753848366!3d18.84394489079891!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bdd2e082b057405%3A0x6194ea41ebffcc79!2sMorya%20Garden%20Mangal%20Karyalaya!5e0!3m2!1sen!2sin!4v1704392840592!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" allowfullscreen="" referrerpolicy="no-referrer-when-downgrade"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d241662.16175808158!2d73.80454753848366!3d18.84394489079891!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bdd2e082b057405%3A0x6194ea41ebffcc79!2sMorya%20Garden%20Mangal%20Karyalaya!5e0!3m2!1sen!2sin!4v1704392840592!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade" referrerPolicy="no-referrer-when-downgrade"
           width="300"
           height="200"
           style={{ border: 0 }}
